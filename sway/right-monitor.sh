0de2a4a3687c072faedf2bf5a1550dcbcb4ebfc2
AlexLoitzl <alexander.loitzl@gmail.com>
2025-01-06 16:07:11 +0100

    Prevent issue with single monitor

--- sway/right-monitor.sh
+++ sway/right-monitor.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+# Read display from sway and take last one
 DISPLAY="$(swaymsg -t get_outputs -p | awk '/Output/ && !/disabled/ {print $2}' | tail -1)"
+# Remove last line
 sed -i '$ d' $HOME/dotfiles/sway/vars.toml
-echo "right = \"$DISPLAY\"" >> $HOME/dotfiles/sway/vars.toml
+
+# If the display is the laptop's monitor, use some dummy instead
+if [ "$DISPLAY" = "eDP-1" ]; then
+  echo "right = \"HDMI-A-1\"" >> $HOME/dotfiles/sway/vars.toml
+else
+  echo "right = \"$DISPLAY\"" >> $HOME/dotfiles/sway/vars.toml
+fi
 
