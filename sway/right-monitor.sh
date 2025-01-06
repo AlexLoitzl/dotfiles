6a131224a6d4a67b1b0d315ba2ff50e15738634b
AlexLoitzl <alexander.loitzl@gmail.com>
2025-01-06 16:51:16 +0100

    Fix profiles

--- sway/right-monitor.sh
+++ sway/right-monitor.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
+# Config file expected as first argument
+CONFIG=$1
+
 # Read display from sway and take last one
 DISPLAY="$(swaymsg -t get_outputs -p | awk '/Output/ && !/disabled/ {print $2}' | tail -1)"
 # Remove last line
-sed -i '$ d' $HOME/dotfiles/sway/vars.toml
+sed -i '$ d' $CONFIG
 
 # If the display is the laptop's monitor, use some dummy instead
 if [ "$DISPLAY" = "eDP-1" ]; then
-  echo "right = \"HDMI-A-1\"" >> $HOME/dotfiles/sway/vars.toml
+  echo "right = \"HDMI-A-1\"" >> $CONFIG 
 else
-  echo "right = \"$DISPLAY\"" >> $HOME/dotfiles/sway/vars.toml
+  echo "right = \"$DISPLAY\"" >> $CONFIG 
 fi
 
