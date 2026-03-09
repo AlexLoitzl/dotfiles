029d17e4ef653f02a9d8a796a1fcac41a9bba4c6
AlexLoitzl <alexander.loitzl@gmail.com>
2026-03-09 11:39:26 +0100

    Update init, sway and XCompose

--- doom/init.el
+++ doom/init.el
@@ -152,7 +152,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ocaml             ; an objective camel
-       ;;org               ; organize your plain life in plain text
+       (org +hugo)       ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
@@ -172,7 +172,7 @@
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        ;;web               ; the tubes
-       ;;yaml              ; JSON, but readable
+       yaml              ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
