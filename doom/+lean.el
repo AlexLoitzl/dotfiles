f2a7bf4f7543656d036a2ed3631914416e66c016
AlexLoitzl <alexander.loitzl@gmail.com>
2026-09-11 16:40:58 +0200

    Fix lean setup

--- doom/+lean.el
+++ doom/+lean.el
@@ -1,4 +1,4 @@
-;; Add column indicator
 (add-hook! nael-mode
   (setq-local display-fill-column-indicator-column 100)
-  (display-fill-column-indicator-mode +1))
+  (display-fill-column-indicator-mode +1)
+  (eglot-ensure))
