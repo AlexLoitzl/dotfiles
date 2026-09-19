3d81b369237adeafef48c21a110e37934f657048
AlexLoitzl <alexander.loitzl@gmail.com>
2026-09-19 20:04:27 +0200

    Add more lean config

--- doom/+lean.el
+++ doom/+lean.el
@@ -1,4 +1,9 @@
 (add-hook! nael-mode
   (setq-local display-fill-column-indicator-column 100)
   (display-fill-column-indicator-mode +1)
-  (eglot-ensure))
+  (eglot-ensure)
+  ;; Change commenting style to use single line comments
+  (setq-local comment-start "--")
+  (setq-local comment-end ""))
+
+;;; +lean.el ends here
